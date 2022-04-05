063476d2108559f8e2e9a2e347a0c1e434931daa
i332371 <premnavin2000@gmail.com>
2022-04-05 15:40:05 +0530

    conditions for address * S/4HANA Mock server updates

--- tests/mocks/srv/external/API_BUSINESS_PARTNER.js
+++ tests/mocks/srv/external/API_BUSINESS_PARTNER.js
@@ -3,15 +3,15 @@ module.exports = async srv => {
     
     const messaging = await cds.connect.to('messaging')
     // Mock events for s4
-    srv.after("CREATE", data => {
-        const payload = {KEY: [{BUSINESSPARTNER: data.BusinessPartner}]};
-        messaging.emit("refapps/bpems/abc/S4H/BO/BusinessPartner/Created", payload);
+    srv.after("CREATE", "A_BusinessPartner", data => {
+        const payload = {"BusinessPartner": data.BusinessPartner}
+        messaging.emit("sap.s4.beh.businesspartner.v1.BusinessPartner.Created.v1", payload);
         console.log('<< event emitted', payload);
     });
 
-    srv.after("UPDATE", data => {
-        const payload = {KEY: [{BUSINESSPARTNER: data.BusinessPartner}]};
-        messaging.emit("refapps/bpems/abc/S4H/BO/BusinessPartner/Changed", payload);
+    srv.after("UPDATE", "A_BusinessPartner", data => {
+        const payload = {"BusinessPartner": data.BusinessPartner}
+        messaging.emit("sap.s4.beh.businesspartner.v1.BusinessPartner.Changed.v1", payload);
         console.log('<< event emitted', payload);
     });
 }
